1fd73df7911343cd4cce0d31b58af0b83edd19d3
reneta_dimitrova <reneta.dimitrova@dev-craft.com>
2026-09-16 14:27:05 +0300

    Configure JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,6 +93,12 @@ DATABASES = {
 
 AUTH_USER_MODEL = "users.User"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
