27f79b6d9eff94d01824bbe713bc9d5d3fe33b6b
reneta_dimitrova <reneta.dimitrova@dev-craft.com>
2026-09-21 12:54:30 +0300

    Optimize idea vote count queries

--- backend/ideas/mixins.py
+++ backend/ideas/mixins.py
@@ -1,7 +1,14 @@
+from django.db.models import Count
+
 from ideas.models import Idea
 
 
 class CompanyIdeaQuerysetMixin:
     def get_queryset(self):
-        user_ideas = Idea.objects.filter(company=self.request.user.company)
+        user_ideas = (
+            Idea.objects
+            .filter(company=self.request.user.company)
+            .annotate(votes_count=Count("idea_votes"))
+        )
+
         return user_ideas
--- backend/ideas/serializers.py
+++ backend/ideas/serializers.py
@@ -4,13 +4,10 @@ from ideas.models import Idea
 
 
 class IdeaSerializer(serializers.ModelSerializer):
-    votes_count = serializers.SerializerMethodField()
+    votes_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Idea
         fields = ['id', 'company', 'created_by', 'title', 'description', 'status', 'created_at', 'updated_at',
                   'votes_count']
         read_only_fields = ['id', 'company', 'created_by', 'created_at', 'updated_at', 'votes_count']
-
-    def get_votes_count(self, obj):
-        return obj.idea_votes.count()
